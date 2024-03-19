69a1e8b190f7ca13ed4f0a8f8368a145050d30bc
Chahiim <2020152199@ub.edu.bz>
2024-03-19 17:00:10 -0600

    Test 2 commit
    Added logo to homepage.
    Deleted sidenav component files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidenavComponent } from "./materials/sidenav/sidenav.component";
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, SidenavComponent]
+    imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'BGLL-Update-Portal';
